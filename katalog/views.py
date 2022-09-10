32ee7991f7ed5be274356a99231d32ee6ba22152
vinsensius.ferdinando <vinsensius.ferdinando@ui.ac.id>
2022-09-10 10:23:49 +0700

    initial commit for tugas1

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+# Create your views here.
+def show_katalog(request):
+    catalog_item = CatalogItem.objects.all()
+    context = {
+        'list_barang': catalog_item,
+        'nama': 'Vinsensius Ferdinando'
+    }
+    return render(request, "katalog.html", context)
