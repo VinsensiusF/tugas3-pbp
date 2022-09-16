c7b81d455001ff32e07cb662870dbf7acb69dc96
vinsensius.ferdinando <vinsensius.ferdinando@ui.ac.id>
2022-09-16 15:26:27 +0700

    task done, need README.md

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class MyWishList(models.Model):
+class MyWatchList(models.Model):
     watched = models.TextField()
     title = models.CharField(max_length=255)
     rating = models.IntegerField()
--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from mywatchlist.models import MyWatchList
+from django.http import HttpResponse
+from django.core import serializers
 
 # Create your views here.
+def show_watchlist_html(request):
+    film_item = MyWatchList.objects.all()
+    context = {
+        'list_film': film_item,
+        'nama': 'Vinsensius Ferdinando',
+        'npm': '2106751221'
+    }
+    return render(request, "mywatchlist.html", context)
+
+def show_xml(request):
+    data = MyWatchList.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = MyWatchList.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_json_by_id(request, id):
+    data = MyWatchList.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = MyWatchList.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
